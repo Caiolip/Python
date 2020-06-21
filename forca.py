33ebd8f59b1164c09486356d30eb6a6d6d393eb8
Caio Dias <63746188+Caiolip@users.noreply.github.com>
2020-06-21 18:34:12 -0300

    melhorando_strings

--- forca.py
+++ forca.py
@@ -8,10 +8,11 @@ def jogar():
     acertou = False
     while (not enforcou and not acertou):
         chute = input("Qual a letra? ")
+        chute = chute.strip()
 
         index = 0
         for letra in palavra_chave:
-            if (chute == letra):
+            if (chute.upper() == letra.upper()):
                 print(("Encontrei a letra {} na posição {}".format(letra, index)))
                 index = index + 1
         print("Jogando ")
